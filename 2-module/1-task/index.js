2e7c469a1ccf5c45320e446b31a531fe96477159
Stanislav Larin <larin.stan@gmail.com>
2020-05-23 23:21:52 +0300

    Module 2.Task 1 - function sumSalaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,21 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
+function sumSalary(salaries) {	
+  let sum = 0;
+  for ( key in salaries ) {
+  	if ( typeof salaries[key] == 'number' ) {
+  		sum += salaries[key];
+  	}
+  }
+  return sum;
+}
 
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
